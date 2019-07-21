815fb4be69ef00b0d579529f10d967f70d379981
iskander <neshput@gmail.com>
2019-07-21 17:00:33 -0400

    first commit

---
+++ src/myfristjava/Dddd.java
@@ -0,0 +1,27 @@
+package myfristjava;
+
+public class Dddd {
+	public static void main(String[] args) {
+//	String continue
+//	String new
+	boolean hhh= false;
+	char mychar='\u0000';
+	byte nnn=100;
+	
+	double doubleValue=5.3;
+	System.out.println(doubleValue);
+	int somelnt =(int)doubleValue;
+	System.out.println(somelnt);
+	double aValue = doubleValue;
+	System.out.println(aValue);
+	double doubleNumber=8.7;
+	System.out.println(doubleNumber);
+	
+	
+	
+	
+	
+	
+
+}
+}
